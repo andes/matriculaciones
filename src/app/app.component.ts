f6a4cb7a606de69b9f45f5d6575698edbf57f462
Hugo <hfernandez@neuquen.gov.ar>
2017-10-12 02:21:43 -0300

    fixes varios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { environment } from './../environments/environment';
 // import { MenuItem } from '@andes/plex/src/lib/app/menu-item.class';
 import { DropdownItem } from '@andes/plex';
 import { Server } from '@andes/shared';
+import { Auth } from '@andes/auth';
 
 
 @Component({
@@ -14,8 +15,52 @@ import { Server } from '@andes/shared';
 })
 export class AppComponent implements OnInit {
 
-    constructor(public _plex: Plex, public _server: Server) {
+    constructor(public plex: Plex, public _server: Server, public auth: Auth) {
         _server.setBaseURL(environment.API);
+         // Inicializa el menu
+         this.checkPermissions();
+    }
+
+    private menuList = [];
+
+    public checkPermissions(): any {
+        let accessList = [];
+        this.menuList = [];
+
+        if (this.auth.loggedIn()) {
+            this.auth.organizaciones().subscribe(data => {
+                if (data.length > 1) {
+                    this.menuList = [{ label: 'Seleccionar organización', icon: 'home', route: '/selectOrganizacion' }, ...this.menuList];
+                    this.plex.updateMenu(this.menuList);
+                }
+            });
+        }
+        // Cargo el array de permisos
+        if (this.auth.getPermissions('turnos:planificarAgenda:?').length > 0) {
+            accessList.push({ label: 'CITAS: Gestor de Agendas y Turnos', icon: 'calendar', route: '/citas/gestor_agendas' });
+        }
+        if (this.auth.getPermissions('turnos:darTurnos:?').length > 0) {
+            accessList.push({ label: 'CITAS: Punto de Inicio', icon: 'calendar', route: '/puntoInicioTurnos' });
+        }
+        if (this.auth.getPermissions('mpi:?').length > 0) {
+            accessList.push({ label: 'MPI: Indice Maestro de Pacientes', icon: 'account-multiple-outline', route: '/mpi' });
+        }
+
+        if (this.auth.getPermissions('rup:?').length > 0) {
+            accessList.push({ label: 'RUP: Registro Universal de Prestaciones', icon: 'contacts', route: '/rup' });
+        }
+
+        this.menuList.push({ label: 'Página principal', icon: 'home', route: '/inicio' });
+
+        accessList.forEach((permiso) => {
+            this.menuList.push(permiso);
+        });
+        this.menuList.push({ divider: true });
+        this.menuList.push({ label: 'Cerrar Sesión', icon: 'logout', route: '/login' });
+
+        // Actualizamos la lista de menú
+        this.plex.updateMenu(this.menuList);
+        return accessList;
     }
 
     ngOnInit() {
@@ -30,7 +75,7 @@ export class AppComponent implements OnInit {
             { label: 'Numeraciones', icon: 'book', route: 'listadoNumeraciones/'},
         ];
 
-        this._plex.initView('Matriculaciones', menu); // .initStaticItems(menu);//.initView('Matriculaciones', menu);
+        this.plex.initView('Matriculaciones', menu); // .initStaticItems(menu);//.initView('Matriculaciones', menu);
     }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,9 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { Plex } from '@andes/plex';
 import { Observable } from 'rxjs/Rx';
-import { Component, ViewEncapsulation } from '@angular/core';
 import { Auth } from '@andes/auth';
+import { AppComponent } from '../../app.component';
 
 @Component({
     templateUrl: 'home.html',
@@ -11,23 +13,30 @@ import { Auth } from '@andes/auth';
 export class HomeComponent {
     public usuario: number;
     public password: string;
-    public organizacion: any;
+    public loading = false;
+    public deshabilitar = false;
+    public autoFocus = 1;
 
-    constructor(private plex: Plex, private auth: Auth) {
+    constructor(private plex: Plex, private auth: Auth, private router: Router) {
     };
 
     login(event) {
         if (event.formValid) {
-            this.auth.login(this.usuario.toString(), this.password, this.organizacion.id)
+            this.deshabilitar = true;
+            this.loading = true;
+            this.auth.login(this.usuario.toString(), this.password)
                 .subscribe((data) => {
-                    this.password = null;
+                    debugger;
+                    this.plex.updateUserInfo({usuario: this.auth.usuario});
+                    this.router.navigate(['selectOrganizacion']);
                 }, (err) => {
+                    debugger;
                     this.plex.alert('Usuario o contraseña incorrectos');
+                    this.loading = false;
+                    this.deshabilitar = false;
                 });
         }
     }
 
-    loadOrganizaciones(event) {
-        this.auth.organizaciones().subscribe(event.callback);
-    }
+ 
 }

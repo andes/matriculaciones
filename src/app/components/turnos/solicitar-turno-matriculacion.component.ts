871714e55b276ed6359781be25f84b8ef5d80d7f
Juliana <juli.otero01@gmail.com>
2019-10-31 15:55:58 -0300

    refactor(matriculaciones): deshabilitar botón de continuar cuando el botón de horario elegido no está activo

--- src/app/components/turnos/solicitar-turno-matriculacion.component.ts
+++ src/app/components/turnos/solicitar-turno-matriculacion.component.ts
@@ -18,83 +18,97 @@ import { PDFUtils } from './../../utils/PDFUtils';
 import * as Enums from './../../utils/enumerados';
 import { Router } from '@angular/router';
 
+const diasSemana = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado'];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 const jsPDF = require('jspdf');
 
 @Component({
-    selector: 'app-solicitar-turno-matriculacion',
-    templateUrl: 'solicitar-turno-matriculacion.html'
+  selector: 'app-solicitar-turno-matriculacion',
+  templateUrl: 'solicitar-turno-matriculacion.html'
 })
 export class SolicitarTurnoMatriculacionComponent implements OnInit {
-    @HostBinding('class.plex-layout') layout = true;  // Permite el uso de flex-box en el componente
-    public tipoTurno: Enums.TipoTurno;
-    private formTurno: FormGroup;
-    public turnoSeleccionado: boolean;
-    public turnoGuardado: boolean;
-    private _turnoSeleccionado: Date;
-    public _nuevoProfesional: any;
+  @HostBinding('class.plex-layout') layout = true;  // Permite el uso de flex-box en el componente
+  public tipoTurno: Enums.TipoTurno;
+  private formTurno: FormGroup;
+  public turnoSeleccionado: boolean;
+  public turnoGuardado: boolean;
+  private _turnoSeleccionado: Date;
+  public _nuevoProfesional: any;
+  lblTurno: string;
+  constructor(private _formBuilder: FormBuilder,
+    private _turnosService: TurnoService,
+    private router: Router,
+    private _pdfUtils: PDFUtils,
+    private plex: Plex) {
 
-    constructor(private _formBuilder: FormBuilder,
-        private _turnosService: TurnoService,
-        private router: Router,
-        private _pdfUtils: PDFUtils,
-        private plex: Plex) {
+    this.tipoTurno = Enums.TipoTurno.matriculacion;
 
-        this.tipoTurno = Enums.TipoTurno.matriculacion;
+  }
 
+  ngOnInit() {
+
+  }
+
+  onTurnoSeleccionado(turno: Date) {
+    this._turnoSeleccionado = turno;
+    this.lblTurno = moment(this._turnoSeleccionado).format('llll');
+    this.lblTurno = diasSemana[this._turnoSeleccionado.getDay()] + ' '
+      + this._turnoSeleccionado.getDate().toString() + ' de '
+      + meses[this._turnoSeleccionado.getMonth()] + ' de '
+      + this._turnoSeleccionado.getFullYear() + ', '
+      + this._turnoSeleccionado.getHours();
+
+    if (this._turnoSeleccionado.getMinutes() > 0) {
+      this.lblTurno += ':' + this._turnoSeleccionado.getMinutes();
     }
 
-    ngOnInit() {
+    this.lblTurno += ' hs';
+    this.turnoSeleccionado = true;
+  }
 
-    }
+  saveTurno() {
 
-    onTurnoSeleccionado(turno: Date) {
-        this._turnoSeleccionado = turno;
-        this.turnoSeleccionado = true;
-    }
+    this.formTurno = this._formBuilder.group({
+      fecha: this._turnoSeleccionado,
+      tipo: this.tipoTurno,
+      profesional: this._nuevoProfesional._id
+    });
 
-    saveTurno() {
+    this._turnosService.saveTurnoMatriculacion({ turno: this.formTurno.value })
+      .subscribe(turno => {
+        const pdf = this._pdfUtils.comprobanteTurno(turno);
+        pdf.save('Turno ' + this._nuevoProfesional.nombre + ' ' + this._nuevoProfesional.apellido + '.pdf');
+      });
+  }
 
-        this.formTurno = this._formBuilder.group({
-            fecha: this._turnoSeleccionado,
-            tipo: this.tipoTurno,
-            profesional: this._nuevoProfesional._id
-        });
+  onProfesionalCompleto(profesional: any) {
 
-        this._turnosService.saveTurnoMatriculacion({ turno: this.formTurno.value })
-            .subscribe(turno => {
-                const pdf = this._pdfUtils.comprobanteTurno(turno);
-                pdf.save('Turno ' + this._nuevoProfesional.nombre + ' ' + this._nuevoProfesional.apellido + '.pdf');
-            });
-    }
-
-    onProfesionalCompleto(profesional: any) {
-
-        const parametros = {
-            documento: profesional.documento,
-            tipoTurno: 'matriculacion',
-            sexo: profesional.sexo
-        };
-        this._turnosService.getTurnosPorDocumento(parametros).subscribe((resultado: any) => {
-            if (resultado.length === 0) {
-                this._turnosService.saveTurnoSolicitados(profesional)
-                    .subscribe((nuevoProfesional) => {
-                        if (nuevoProfesional == null) {
-                            this.plex.info('info', 'El profesional que quiere agregar ya existe(verificar dni)');
-                        } else {
-
-                            this._nuevoProfesional = nuevoProfesional;
-                            this.turnoGuardado = true;
-                            if (this._turnoSeleccionado && this._nuevoProfesional) {
-                                this.saveTurno();
-                            }
-                            this.plex.toast('success', 'Se registro con exito!', 'informacion', 1000);
-                            this.router.navigate(['requisitosGenerales']);
-                        }
-                    });
+    const parametros = {
+      documento: profesional.documento,
+      tipoTurno: 'matriculacion',
+      sexo: profesional.sexo
+    };
+    this._turnosService.getTurnosPorDocumento(parametros).subscribe((resultado: any) => {
+      if (resultado.length === 0) {
+        this._turnosService.saveTurnoSolicitados(profesional)
+          .subscribe((nuevoProfesional) => {
+            if (nuevoProfesional == null) {
+              this.plex.info('info', 'El profesional que quiere agregar ya existe(verificar dni)');
             } else {
-                this.plex.info('info', 'usted ya tiene un turno para el dia <strong>' + moment(resultado[0].fecha).format('DD MMMM YYYY, h:mm a' + '</strong>'));
-                // this.router.navigate(['requisitosGenerales']);
+
+              this._nuevoProfesional = nuevoProfesional;
+              this.turnoGuardado = true;
+              if (this._turnoSeleccionado && this._nuevoProfesional) {
+                this.saveTurno();
+              }
+              this.plex.toast('success', 'Se registro con exito!', 'informacion', 1000);
+              this.router.navigate(['requisitosGenerales']);
             }
-        });
-    }
+          });
+      } else {
+        this.plex.info('info', 'usted ya tiene un turno para el dia <strong>' + moment(resultado[0].fecha).format('DD MMMM YYYY, h:mm a' + '</strong>'));
+        // this.router.navigate(['requisitosGenerales']);
+      }
+    });
+  }
 }

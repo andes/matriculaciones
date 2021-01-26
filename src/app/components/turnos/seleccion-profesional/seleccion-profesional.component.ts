0f28fb59fef5c8845ffa7472a0de7224b3c86057
palita1991 <marianopalavecino7@gmail.com>
2021-01-26 10:29:31 -0300

    feat(MAT): separacion de componentes de horarios y profesional

---
+++ src/app/components/turnos/seleccion-profesional/seleccion-profesional.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seleccion-profesional',
+  templateUrl: './seleccion-profesional.component.html'
+})
+export class SeleccionProfesionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
